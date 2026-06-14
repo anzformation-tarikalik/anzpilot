1dc9757277a1ce8d06b89ed2a7c49db459acc9f8
anzformation-tarikalik <anzformation@gmail.com>
2026-06-14 10:34:42 +0200

    Delete anzpilot-upload directory

--- anzpilot-upload/pages/api/stripe/webhook.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export const config = { api: { bodyParser: false } }
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.json({ received: true })
-}
